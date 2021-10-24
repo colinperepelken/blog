835233af3a8e89f1a7a42302a25e81793d259af0
Colin Perepelken <colin@sparelabs.com>
2021-10-24 13:38:45 -0700

    Add router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Home from './components/screens/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import SinglePost from './components/screens/SinglePost';
 
 const App = () => {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+      <Switch>
+          <Route path="/post">
+            <SinglePost />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
