4dac1a6dc72d96008d21565ec03f5eb54e20010e
Colin Perepelken <colin@sparelabs.com>
2021-10-24 14:38:55 -0700

    Add eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'eslint:recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-use-before-define': 'off',
+    'no-unused-vars': 2,
+    'react/react-in-jsx-scope': 'off'
+  }
+}
