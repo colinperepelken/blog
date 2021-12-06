99ddd23937d435e7ddb0e4ef25fc37acd9927208
Colin Perepelken <colin@sparelabs.com>
2021-12-05 19:49:21 -0800

    Style button

--- client/src/components/shared/Button.tsx
+++ client/src/components/shared/Button.tsx
@@ -1,10 +1,27 @@
+import styled from 'styled-components'
+import { Theme } from '../../features/theme/Theme'
+
 export interface IButtonProps {
     title: string
     onClick?: () => void
+    primary?: boolean
 }
 
+const StyledButton = styled.button<Pick<IButtonProps, 'primary'>>`
+    background-color: ${(props) =>
+        props.primary ? Theme.primary : Theme.secondary};
+    border-radius: 5px;
+    border: none;
+    padding: 5px 10px;
+    cursor: pointer;
+    border: 1px solid transparent;
+    &:hover {
+        border-color: black;
+    }
+`
+
 const Button = (props: IButtonProps) => {
-    return <button>{props.title}</button>
+    return <StyledButton>{props.title}</StyledButton>
 }
 
 export default Button
