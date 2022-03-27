1eb76c24082bee60b42cfbe49bb30128af478083
Colin Perepelken <colin@sparelabs.com>
2022-03-26 17:02:56 -0700

    Add sequelize-typescript package

--- api/src/models/Article.ts
+++ api/src/models/Article.ts
@@ -1,3 +1,30 @@
-export class Article {
+import { IArticleDetails } from "@blog/shared-types";
+import { AutoIncrement, Column, CreatedAt, DataType, Model, PrimaryKey, Table, UpdatedAt } from "sequelize-typescript";
+
+@Table({ timestamps: true })
+export class Article extends Model<Article> implements IArticleDetails{
+
+    @AutoIncrement
+    @PrimaryKey
+    @Column
+    public id!: number;
+
+    @Column(DataType.TEXT)
+    public title!: string;
+
+    @Column(DataType.TEXT)
+    public imageUrl!: string;
+
+    @Column(DataType.TEXT)
+    public content!: string;
+
+    @Column(DataType.TEXT)
+    public author!: string;
+
+    @CreatedAt
+    public createdAt!: number
+
+    @UpdatedAt
+    public updatedAt!: number
     
 }
