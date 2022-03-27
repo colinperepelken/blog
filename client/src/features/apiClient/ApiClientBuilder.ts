87933242f4de143aca45490d44b78fb6cae9977c
Colin Perepelken <colin@sparelabs.com>
2022-03-27 12:15:15 -0700

    Create OAS file and refactor api client in blog client

---
+++ client/src/features/apiClient/ApiClientBuilder.ts
@@ -0,0 +1,15 @@
+import OpenAPIClientAxios, { Document } from 'openapi-client-axios'
+import oasFile from '../../../../oas/reference/Blog.json'
+import { Client as ApiClient } from './ApiClient'
+
+export class ApiClientBuilder {
+    public static async build(): Promise<ApiClient> {
+        const api = new OpenAPIClientAxios({
+            definition: oasFile as Document,
+        })
+        api.init()
+        const client = await api.getClient<ApiClient>()
+
+        return client
+    }
+}
