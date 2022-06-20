9e61e41a5b9a443ef5af8473ad688705a2f4ffdc
Colin Perepelken <colin@sparelabs.com>
2022-06-19 19:05:29 -0700

    Pop up preview after clicking article circle

--- client/src/components/ArticlePreview.tsx
+++ client/src/components/ArticlePreview.tsx
@@ -2,7 +2,7 @@ import { IArticleDetails } from '@blog/shared-types'
 import { useHistory } from 'react-router-dom'
 import { AuthenticationProvider } from '../features/authentication/AuthenticationProvider'
 import { DateTimeFormatter } from '../features/DateTimeFormatter'
-import { IButtonProps } from './shared/Button'
+import Button, { IButtonProps } from './shared/Button'
 import { Card } from './shared/Card'
 import { useState, useEffect } from 'react'
 import moment from 'moment'
@@ -15,18 +15,13 @@ const ArticlePreview = (props: IProps) => {
     const history = useHistory()
     const [showHoverBox, setShowHoverBox] = useState(false)
     const [[x, y], setPosition] = useState(getNewPosition())
-    return (
-        <div
-            className="absolute bg-green-500 w-16 h-16 rounded-full opacity-80 hover:opacity-90 shadow-lg hover:shadow-2xl cursor-pointer border-2 border-slate-700 hover:border-slate-900 hover:border-2"
-            style={{ left: x, top: y }}
-            onMouseOver={() => setShowHoverBox(true)}
-            onMouseOut={() => setShowHoverBox(false)}
-        >
-            {showHoverBox && (
-                <div
-                    className="bg-slate-200 absolute p-1 rounded-sm text-slate-800"
-                    style={{ left: 80, top: 5 }}
-                >
+    if (showHoverBox) {
+        return (
+            <div
+                className="bg-green-900 absolute w-48 h-48 p-1 rounded-full text-slate-800 text-center flex items-center z-10"
+                style={{ left: x, top: y }}
+            >
+                <div>
                     <h2 className="text-lg font-semibold">
                         {props.article.title}
                     </h2>
@@ -37,17 +32,30 @@ const ArticlePreview = (props: IProps) => {
                             .format('MMMM Do, YYYY')}{' '}
                         by {props.article.author}
                     </p>
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-1"
+                        onClick={() => setShowHoverBox(false)}
+                    >
+                        X
+                    </button>
                 </div>
-            )}
-        </div>
-
-        // <Card
-        //     title={props.article.title}
-        //     onClick={() => history.push(`/article/${props.article.id}/view`)}
-        //     buttons={getAvailableButtons(props.article)}
-        //     content={getContent(props.article)}
-        // ></Card>
+            </div>
+        )
+    }
+    return (
+        <div
+            className="absolute bg-green-900 w-16 h-16 rounded-full opacity-80 hover:opacity-90 shadow-lg hover:shadow-2xl cursor-pointer border-2 border-slate-700 hover:border-slate-900 hover:border-2 z-40"
+            style={{ left: x, top: y }}
+            onClick={() => setShowHoverBox(true)}
+        ></div>
     )
+
+    // <Card
+    //     title={props.article.title}
+    //     onClick={() => history.push(`/article/${props.article.id}/view`)}
+    //     buttons={getAvailableButtons(props.article)}
+    //     content={getContent(props.article)}
+    // ></Card>
 }
 
 const getContent = (article: IArticleDetails) => (
