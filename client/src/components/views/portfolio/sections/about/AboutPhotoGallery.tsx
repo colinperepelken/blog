d0a0eb86f4ac42a6613123439b9db62a34024ab4
Colin Perepelken <colin@sparelabs.com>
2022-10-29 13:21:19 -0700

    Add parallax to photo gallery

--- client/src/components/views/portfolio/sections/about/AboutPhotoGallery.tsx
+++ client/src/components/views/portfolio/sections/about/AboutPhotoGallery.tsx
@@ -1,52 +1,61 @@
+import { Parallax } from 'react-scroll-parallax'
+
 export const AboutPhotoGallery = () => (
     <div className="container h-full">
-        <div className="flex flex-wrap h-full">
-            <div className="flex flex-wrap w-1/2">
-                <div className="w-1/2 p-1 md:p-2">
-                    <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded"
-                        src="assets/gallery/gallery1.jpeg"
-                    />
+        <Parallax
+            className="h-full"
+            opacity={[0, 1]}
+            startScroll={100}
+            endScroll={800}
+        >
+            <div className="flex flex-wrap h-full">
+                <div className="flex flex-wrap w-1/2">
+                    <div className="w-1/2 p-1 md:p-2">
+                        <img
+                            alt="gallery"
+                            className="block object-cover object-center w-full h-full rounded"
+                            src="assets/gallery/gallery1.jpeg"
+                        />
+                    </div>
+                    <div className="w-1/2 p-1 md:p-2">
+                        <img
+                            alt="gallery"
+                            className="block object-cover object-center w-full h-full rounded"
+                            src="assets/gallery/gallery2.jpeg"
+                        />
+                    </div>
+                    <div className="w-full p-1 md:p-2">
+                        <img
+                            alt="gallery"
+                            className="block object-cover object-center w-full h-full rounded"
+                            src="assets/gallery/gallery3.jpeg"
+                        />
+                    </div>
                 </div>
-                <div className="w-1/2 p-1 md:p-2">
-                    <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded"
-                        src="assets/gallery/gallery2.jpeg"
-                    />
-                </div>
-                <div className="w-full p-1 md:p-2">
-                    <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded"
-                        src="assets/gallery/gallery3.jpeg"
-                    />
+                <div className="flex flex-wrap w-1/2">
+                    <div className="w-full p-1 md:p-2">
+                        <img
+                            alt="gallery"
+                            className="block object-cover object-center w-full h-full rounded"
+                            src="assets/gallery/gallery4.jpeg"
+                        />
+                    </div>
+                    <div className="w-1/2 p-1 md:p-2">
+                        <img
+                            alt="gallery"
+                            className="block object-cover object-center w-full h-full rounded"
+                            src="assets/gallery/gallery5.jpeg"
+                        />
+                    </div>
+                    <div className="w-1/2 p-1 md:p-2">
+                        <img
+                            alt="gallery"
+                            className="block object-cover object-center w-full h-full rounded"
+                            src="assets/gallery/gallery6.jpeg"
+                        />
+                    </div>
                 </div>
             </div>
-            <div className="flex flex-wrap w-1/2">
-                <div className="w-full p-1 md:p-2">
-                    <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded"
-                        src="assets/gallery/gallery4.jpeg"
-                    />
-                </div>
-                <div className="w-1/2 p-1 md:p-2">
-                    <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded"
-                        src="assets/gallery/gallery5.jpeg"
-                    />
-                </div>
-                <div className="w-1/2 p-1 md:p-2">
-                    <img
-                        alt="gallery"
-                        className="block object-cover object-center w-full h-full rounded"
-                        src="assets/gallery/gallery6.jpeg"
-                    />
-                </div>
-            </div>
-        </div>
+        </Parallax>
     </div>
 )
