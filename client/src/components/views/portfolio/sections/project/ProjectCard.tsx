03ffde84b8cad861155437e9ea6c8715e8000966
Colin Perepelken <colin@sparelabs.com>
2022-11-06 15:58:36 -0800

    Add more projects

--- client/src/components/views/portfolio/sections/project/ProjectCard.tsx
+++ client/src/components/views/portfolio/sections/project/ProjectCard.tsx
@@ -1,25 +1,18 @@
-interface IProps {
+export interface IProjectCardProps {
     title: string
     description: string
+    image: string
     githubUrl: string
     liveUrl?: string
 }
 
-export const ProjectCard = (props: IProps) => (
+export const ProjectCard = (props: IProjectCardProps) => (
     <div className="max-w-sm bg-black rounded border border-slate-500 hover:border-slate-400">
-        <a href="#">
-            <img
-                className="rounded-t-lg"
-                src="/docs/images/blog/image-1.jpg"
-                alt=""
-            />
-        </a>
+        <img src={props.image} alt="Project image" />
         <div className="p-5">
-            <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    {props.title}
-                </h5>
-            </a>
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {props.title}
+            </h5>
             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                 {props.description}
             </p>
@@ -28,24 +21,11 @@ export const ProjectCard = (props: IProps) => (
                 className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
                 Source
-                <svg
-                    aria-hidden="true"
-                    className="ml-2 -mr-1 w-4 h-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                    ></path>
-                </svg>
             </a>
             {props.liveUrl && (
                 <a
                     href={props.liveUrl}
-                    className="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="inline-flex items-center ml-2 py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     Live
                 </a>
--- client/src/components/views/portfolio/sections/project/ProjectSection.tsx
+++ client/src/components/views/portfolio/sections/project/ProjectSection.tsx
@@ -1,10 +1,60 @@
 import ProjectCard from './ProjectCard'
+import { IProjectCardProps } from './ProjectCard'
+
+const projects: IProjectCardProps[] = [
+    {
+        title: 'bikegears.ca',
+        description:
+            "This website allows cyclists to compute gear ratios, gear inches and several other useful calculations. It provides dynamic graphs, auto-generated insights, and table data to help users choose suitable gearing for their bikes. Users can compare results for multiple bikes at the same time, and can read more about each calculation by visiting the 'Learn' section.",
+        githubUrl: 'https://github.com/colinperepelken/bikegears',
+        liveUrl: 'https://bikegears.ca',
+        image: 'assets/projects/bike-gears.png',
+    },
+    {
+        title: 'perepelken.ca',
+        description:
+            'My portfolio website that you are currently viewing. I have also created a Blog, where I plan to write about my hobbies, software development, and anything else that captures my interest.',
+        githubUrl: 'https://github.com/colinperepelken/blog',
+        liveUrl: 'https://perepelken.ca',
+        image: 'assets/projects/blog.png',
+    },
+    {
+        title: 'Portfolio Website v1',
+        description: 'My previous portfolio website that is no longer live.',
+        githubUrl: 'https://github.com/colinperepelken/perepelken.ca',
+        image: 'assets/projects/old-portfolio.png',
+    },
+    {
+        title: 'LTI Provider for Canadian School Districts',
+        description:
+            'A Learning Tools Interoperability (LTI) provider that allows access to elementary through highschool courses to school districts across Western Canada. The provider handles millions of student requests for resources every year and has been integrated into Moodle, Canvas, and D2L.',
+        githubUrl: 'https://github.com/wcln/moodle-local_lti',
+        image: 'assets/projects/lti.png',
+    },
+    {
+        title: 'Learning Object Repository',
+        description:
+            'The Learning Object Repository was built using Vue.js to store and manage 5000+ games, projects, video tutorials and other resources which can be embedded in courses by teachers to assist with online learning.',
+        githubUrl: 'https://github.com/wcln/moodle-local_lor',
+        liveUrl: 'https://wcln.ca/local/lor/index.php/resources',
+        image: 'assets/projects/lor.png',
+    },
+    {
+        title: 'RSA Cryptosystem in Python',
+        description:
+            'A Python implementation of the RSA cryptosystem. Created for my own learning purposes. Capable of generating extremely simple and breakable (but functionally correct) public and private keys. These keys can be used from the command line to encrypt and decrypt files.',
+        githubUrl: 'https://github.com/colinperepelken/python-rsa',
+        image: 'assets/projects/python-rsa.png',
+    },
+]
 
 export const ProjectSection = () => (
     <div className="mx-auto w-full p-6 to-black from-[#220930] bg-gradient-to-b">
         <h3 className="text-3xl font-extralight text-center mb-10">Projects</h3>
         <div className="grid grid-cols-3 gap-4 justify-center">
-            <ProjectCard title="Project 1" description="" githubUrl="" />
+            {projects.map((project) => (
+                <ProjectCard key={project.title} {...project} />
+            ))}
         </div>
     </div>
 )
