562c8c1db9d2215e596009638f90410f3cff227e
Colin Perepelken <colin@perepelken.ca>
2024-11-21 08:29:09 -0800

    Shorten about

--- client/src/components/views/portfolio/sections/about/AboutSection.tsx
+++ client/src/components/views/portfolio/sections/about/AboutSection.tsx
@@ -11,31 +11,17 @@ export const AboutSection = () => {
                     </h3>
                     <div className="leading-relaxed font-extralight text-black p-2 space-y-2">
                         <p>
-                            An enthusiastic software engineer who strives to
-                            make the world a better, more sustainable place.
-                            I&apos;m passionate about contributing to solutions
-                            that derive real-world change, and positively affect
+                            A results driven full-stack Staff Software Engineer
+                            with 6+ years of professional experience. Values
+                            roles focused on driving long-term success, and
+                            crafting impactful features that positively affect
                             the lives of others.
                         </p>
                         <p>
-                            I am very lucky to live in beautiful Kelowna, BC
-                            with my wonderful wife and my energetic dog Goose. I
-                            graduated from the University of British Columbia
-                            (UBCO) in 2018 and have been enjoying my work as a
-                            software engineer since.
-                        </p>
-                        <p>
-                            I love riding my bike everywhere I can — to the
-                            grocery store, on camping trips, and mountain biking
-                            in the surrounding hills. I am passionate about
-                            bicycles and alternative methods of transportation
-                            as means to improve the lives of others and to save
-                            the environment.
-                        </p>
-                        <p>
-                            I am currently working with Spare to build the
-                            future of sustainable transportation using Node.js,
-                            React.js, and TypeScript.
+                            Currently working with Spare to support transit
+                            agencies by providing innovative technology for
+                            paratransit and microtransit services, empowering
+                            over 10 million rides globally.
                         </p>
                     </div>
                 </div>
