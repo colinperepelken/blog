fbbf984364a5769a0ee43b9b2621126cd7a40c57
Colin Perepelken <colin@perepelken.ca>
2024-11-21 08:26:24 -0800

    Update avatar image

--- client/src/components/common/Avatar.tsx
+++ client/src/components/common/Avatar.tsx
@@ -1,7 +1,7 @@
 export const Avatar = () => (
     <img
         className="inline-block h-36 w-36 rounded-full ring-8 ring-primary-300 shadow-md"
-        src="assets/avatar.png"
+        src="assets/avatar.jpg"
         alt=""
     />
 )
